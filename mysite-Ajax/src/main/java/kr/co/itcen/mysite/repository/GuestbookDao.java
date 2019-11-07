914696796633fe2cd6e53848ae37428d98e120e9
forcetyty <forcetyty@gmail.com>
2019-11-07 16:04:21 +0900

    MySite - Ajax

---
+++ mysite-Ajax/src/main/java/kr/co/itcen/mysite/repository/GuestbookDao.java
@@ -0,0 +1,44 @@
+package kr.co.itcen.mysite.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.co.itcen.mysite.vo.GuestbookVo;
+
+@Repository
+public class GuestbookDao {
+	@Autowired
+	private SqlSession sqlSession;
+
+	public int delete( GuestbookVo vo ) {
+		int count = sqlSession.delete( "guestbook.delete", vo );
+		return count;
+	}
+	
+	public int insert( GuestbookVo vo ) {
+		return sqlSession.insert( "guestbook.insert", vo );
+	}
+
+	public List<GuestbookVo> getList(Long startNo){
+		List<GuestbookVo> result = sqlSession.selectList("guestbook.getList2", startNo);
+		return result;
+	}
+	
+	public List<GuestbookVo> getList(){
+		List<GuestbookVo> result = sqlSession.selectList( "guestbook.getList" );
+		return result;
+	}
+
+	public int delete(Long no, String password) {
+		GuestbookVo vo = new GuestbookVo();
+		vo.setNo(no);
+		vo.setPassword(password);
+		
+		return delete(vo);
+	}
+	
+	
+}
